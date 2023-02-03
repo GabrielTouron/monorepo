8eb6fb32d1ee0fb384d5b718fd6b2f48cf0f7b17
Gabriel Touron <gtcode@pm.me>
2023-02-03 18:05:49 +0100

    fix backend project json to have prisma

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -13,7 +13,7 @@ app.get('/', async () => {
 
 app.get('/posts', async () => {
   const posts = await prisma.post.findMany();
-  console.log('new stuff12');
+  console.log('new stuff13');
   return { posts };
 });
 
