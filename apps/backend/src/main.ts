88ced5f04c72c7e8eee89573e3193585325947c5
Gabriel Touron <gtcode@pm.me>
2023-02-03 15:37:14 +0100

    edit backend to test CI

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -13,7 +13,7 @@ app.get('/', async () => {
 
 app.get('/posts', async () => {
   const posts = await prisma.post.findMany();
-  console.log('new stuff2');
+  console.log('new stuff4');
   return { posts };
 });
 
