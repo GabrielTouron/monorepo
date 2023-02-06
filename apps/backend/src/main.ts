65bfd3680209184b798233786820824603f65238
Gabriel Touron <gtcode@pm.me>
2023-02-06 14:47:08 +0100

    test affect docker

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -13,7 +13,7 @@ app.get('/', async () => {
 
 app.get('/posts', async () => {
   const posts = await prisma.post.findMany();
-  console.log('new stuff23');
+  console.log('new stuff24');
   return { posts };
 });
 
